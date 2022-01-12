cb40b4ce9662868a22983ce925030c25ecc69b71
Adam Donaghy <donaghyam@gmail.com>
2022-01-12 14:16:45 -0600

    1st function working

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-const quarterValue = () => {
-
-
+const quarterValue = (number) => {
+    const quarter = number / 4
+    return quarter
 }
+
+const quarterNumber = quarterValue(99)
 
 // Invoke the function and store the return value
 
 
 // Log the return value
-console.log()
+console.log(quarterNumber)
